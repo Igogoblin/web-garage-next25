6f43fda763ff1214668d3a955fa23bb34fe9af1b
Ihar Skavysh <igogoblin@gmail.com>
2025-07-09 10:01:57 +0300

    feat: add font

---
+++ delivery-shop/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Rubik } from "next/font/google";
+import "./globals.css";
+
+const rubik = Rubik({
+  variable: "--font-rubik",
+  subsets: ["latin", "cyrillic"],
+});
+
+export const metadata: Metadata = {
+  title: "shop",
+  description: "Доставка и покупка продуктов питания",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${rubik.variable} font-sans`}>{children}</body>
+    </html>
+  );
+}
